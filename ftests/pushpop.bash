d66bbf26c95cced674d3f99f5d9dae386c375735
Casey Marshall <cmars@cmarstech.com>
2015-08-02 18:39:08 -0500

    Decided on a license: MPL 2.0.

--- ftests/pushpop.bash
+++ ftests/pushpop.bash
@@ -1,4 +1,12 @@
 #!/bin/bash -ex
+#
+#  Copyright 2015 Casey Marshall.
+#
+#  This Source Code Form is subject to the terms of the Mozilla Public
+#  License, v. 2.0. If a copy of the MPL was not distributed with this
+#  file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+
 
 TESTDIR=$(mktemp -d)
 TRAPEXIT="rm -rf $TESTDIR"
