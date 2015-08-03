97ebf0acfa472fc48db36c3c0226256adeb6b8af
Casey Marshall <cmars@cmarstech.com>
2015-08-02 20:27:21 -0500

    ftest cleanup after itself

--- ftests/pushpop.bash
+++ ftests/pushpop.bash
@@ -8,48 +8,59 @@
 #
 
 
-go build -o sfd github.com/cmars/shadowfax/cmd/sfd
-go build -o sf github.com/cmars/shadowfax/cmd/sf
+TESTDIR=$(mktemp -d)
+TRAPEXIT="rm -rf $TESTDIR"
+trap "$TRAPEXIT" EXIT
+cd $TESTDIR
 
-rm -rf .alice .bob
+mkdir bin
 
-./sfd &
+go build -o bin/sfd github.com/cmars/shadowfax/cmd/sfd
+go build -o bin/sf github.com/cmars/shadowfax/cmd/sf
+
+if [ ! -x bin/basen ]; then
+	go build -o bin/basen gopkg.in/basen.v1/cmd/basen
+fi
+
+bin/sfd &
 SFD_PID=$!
-trap "kill ${SFD_PID}" EXIT
+TRAPEXIT="$TRAPEXIT;kill ${SFD_PID}"  # shitty bash defer
+trap "$TRAPEXIT" EXIT
 sleep 1
 
-SFD_KEY=$(basen -encode 58 <(head -c 32 sfd.keypair))
+SFD_KEY=$(bin/basen -encode 58 <(head -c 32 sfd.keypair))
 if [ -z "$SFD_KEY" ]; then
 	echo "failed to read server key"
 	exit 1
 fi
 
 
-./sf --homedir .alice --passphrase /dev/null addr create
-ALICE_ADDR=$(./sf --homedir .alice --passphrase /dev/null addr default)
+bin/sf --homedir .alice --passphrase /dev/null addr create
+ALICE_ADDR=$(bin/sf --homedir .alice --passphrase /dev/null addr default)
 if [ -z "$ALICE_ADDR" ]; then
 	echo "failed to read alice address"
 	exit 1
 fi
 
 # Bob creates an address
-./sf --homedir .bob --passphrase /dev/null addr create
-BOB_ADDR=$(./sf --homedir .bob --passphrase /dev/null addr default)
+bin/sf --homedir .bob --passphrase /dev/null addr create
+BOB_ADDR=$(bin/sf --homedir .bob --passphrase /dev/null addr default)
 if [ -z "$BOB_ADDR" ]; then
 	echo "failed to read bob address"
 	exit 1
 fi
 
 # Exchange addresses
-./sf --homedir .alice name add bob $BOB_ADDR
-./sf --homedir .bob name add alice $ALICE_ADDR
+bin/sf --homedir .alice name add bob $BOB_ADDR
+bin/sf --homedir .bob name add alice $ALICE_ADDR
 
 # Alice sends Bob a message
 ALICE_MSG=$(mktemp)
-trap "rm -f $ALICE_MSG" EXIT
+TRAPEXIT="$TRAPEXIT;rm -f $ALICE_MSG"
+trap "$TRAPEXIT" EXIT
 echo "hello" > $ALICE_MSG
-./sf --server-key ${SFD_KEY} --url http://localhost:8080 --homedir .alice --passphrase /dev/null msg push bob $ALICE_MSG
+bin/sf --server-key ${SFD_KEY} --url http://localhost:8080 --homedir .alice --passphrase /dev/null msg push bob $ALICE_MSG
 
 # Bob checks messages
-./sf --server-key ${SFD_KEY} --url http://localhost:8080 --homedir .bob --passphrase /dev/null msg pop
+bin/sf --server-key ${SFD_KEY} --url http://localhost:8080 --homedir .bob --passphrase /dev/null msg pop
 
